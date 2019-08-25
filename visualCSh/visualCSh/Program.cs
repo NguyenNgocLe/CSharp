a905e4ba7821f242e719d447dd378b0228d344b6
le <nguyenngocle2408@gmail.com>
2019-08-25 12:26:10 +0700

    no message

---
+++ visualCSh/visualCSh/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace visualCSh
+{
+    class MainClass
+    {
+        public static void a()
+        {
+            Console.WriteLine("Temp");
+        }
+        public static void Main(string[] args)
+        {
+            a();
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
